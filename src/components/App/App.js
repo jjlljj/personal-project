d10caf381b69109b540256fdf85f0d023c4af7d1
James Logue <logueja@gmail.com>
2018-02-13 10:23:28 -0700

    Add folder structure, setup redux, lint, testing

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">PersonalProject</h1>
+        </header>
+        <div>i am app</div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/index.js'
+import logger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+  && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const middleware = applyMiddleware(logger)
+
+const store = createStore(rootReducer, devtools, middleware)
+
+ReactDOM.render(
+  <Provider store = { store } >
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
+  </Provider>, 
+  document.getElementById('root'))
+
+registerServiceWorker()
