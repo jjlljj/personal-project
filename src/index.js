dd3828a18361635f1f2acd2d6978adc5f9f04093
James Logue <logueja@gmail.com>
2018-02-13 10:36:08 -0700

    Fix link in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './components/App'
+import App from './components/App/App'
+import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
@@ -13,7 +14,7 @@ const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
 
 const middleware = applyMiddleware(logger)
 
-const store = createStore(rootReducer, devtools, middleware)
+const store = createStore(rootReducer, devTools, middleware)
 
 ReactDOM.render(
   <Provider store = { store } >
