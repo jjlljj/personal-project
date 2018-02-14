eb3a8847f041c29f36091b209455a2ccb8d71e07
James Logue <logueja@gmail.com>
2018-02-14 12:26:46 -0700

    Update gitignore, begin building initial fetch

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {apiUsername, apiPassword} from 
 import './Main.css'
 
 export class Main extends Component {
 
+  async componentDidMount() {
+   
+
+    console.log('resolved')
+  }
+
+  apiFetch = async () => {
+    const url = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone"
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${apiUsername}:${apiPassword}`
+        "Content-Type": "application/json"
+      }
+
+    })
+    const resolved = await response.json()
+  } 
 
   render() {
     return (
