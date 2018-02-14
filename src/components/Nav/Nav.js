d3c7fc7a1218db7a9f4f529f2bc0a9def39fce39
James Logue <logueja@gmail.com>
2018-02-14 11:46:17 -0700

    Add Nav, Main components, create initial header, render placeholder content

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <header>
+      <h1>
+        <NavLink to="/">
+          SITE HEADER
+        </NavLink>
+      </h1>
+
+      <nav>
+        <div>
+          <NavLink to="/">
+            Linkto
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/">
+            Linkto
+          </NavLink>
+        </div>
+
+      </nav>
+
+    </header>
+  )
+}
+
+export default Nav
