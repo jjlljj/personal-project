012c3a009b0ba2adc19c1eba117f0591f51adb5b
James Logue <logueja@gmail.com>
2018-02-19 13:57:24 -0700

    Add Main snapshot test

---
+++ src/containers/Main/Main.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Main } from './Main'
+
+describe('Main', () => {
+  let renderedComponent
+
+  beforeEach(() => {
+    renderedComponent = shallow(<Main />)
+  })
+
+  it('should match snapshot', () => {
+
+    expect(renderedComponent).toMatchSnapshot()
+  })
+})
