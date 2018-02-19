39cb42dc3f3d7e854c7aa44f9e7333b01f29f32c
James Logue <logueja@gmail.com>
2018-02-19 13:54:57 -0700

    Add App component snapshot

--- src/containers/AnalyzerInput/AnalyzerInput.test.js
+++ src/containers/AnalyzerInput/AnalyzerInput.test.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from 'react'
+import { shallow } from 'enzyme'
 import { AnalyzerInput } from './AnalyzerInput'
 import { mockText, mockResponse } from '../../__mocks__/mockData'
 
