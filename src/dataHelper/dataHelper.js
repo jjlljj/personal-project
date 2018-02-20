f4896b396f293a4f1e2bc310be77bbc605d729f6
James Logue <logueja@gmail.com>
2018-02-20 09:38:59 -0700

    Clean sentences tones and data

--- src/dataHelper/dataHelper.js
+++ src/dataHelper/dataHelper.js
@@ -41,23 +41,48 @@ export const mockFetch = async (text) => {
   }  
 }
 
+// WHAT TO DO WITH SOCIAL TONE BIG5 TONES...
+// currently not pulling them in, but they are there to be used
+
 export const cleanTones = response => {
-  const documentTones = cleanDocumentTone(response)
-  const sentences = cleanSentenceTones(response)
+  const documentTone = cleanDocumentTone(response)
+  const sentences = cleanSentences(response)
+  
+  return { documentTone, sentences }
 }
 
 export const cleanDocumentTone = ({ document_tone }) => {
-  const toneThreshold = 0.2
-  const tones = [ 
-    ...document_tone.tone_categories[0].tones, 
-    ...document_tone.tone_categories[1].tones 
-  ]
-
-  return tones
-    .filter(tone => tone.score >= toneThreshold)
-    .sort((a, b) => b.score - a.score)
+  const toneThreshold = 0.3
+  return cleanTone(document_tone, toneThreshold)
 }
 
-export const cleanSentenceTones = sentences => {
+export const cleanSentences = ({ sentences_tone }) => {
+  const toneThreshold = 0.2
+  return sentences_tone.map(sentence => {
+    let tones = cleanTone(sentence, toneThreshold)
 
+    return { 
+      sentence_id: sentence.sentence_id,
+      text: sentence.text,
+      tones  
+    }
+  })
+}
+
+export const cleanTone = ( data, thresh ) => {
+  const tones = cleanToneCategories(data.tone_categories)
+  return filterAndSort(tones, thresh)
+}
+
+export const cleanToneCategories = categories => {
+  return [
+    ...categories[0].tones,
+    ...categories[1].tones
+  ]
+}
+
+export const filterAndSort = (tones, thresh = 0) => {
+  return tones
+    .filter(tone => tone.score >= thresh)
+    .sort((a, b) => b.score - a.score) 
 }
