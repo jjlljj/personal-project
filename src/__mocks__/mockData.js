9877a9ffdef1e9e436c3c2e9e0af8080d0cc1b5f
James Logue <logueja@gmail.com>
2018-02-20 10:34:52 -0700

    Add tests for dataHelper response cleaner functions

--- src/__mocks__/mockData.js
+++ src/__mocks__/mockData.js
@@ -131,281 +131,288 @@ export const mockCleaned = {
   ]
 }
 
-
-export const mockState = {
-  results: [
-    {
-      document_tone: {
-        tone_categories: [
+export const mockUncleaned = {
+  document_tone: {
+    tone_categories: [
+      {
+        tones: [
           {
-            tones: [
-              {
-                score: 0.036792,
-                tone_id: 'anger',
-                tone_name: 'Anger'
-              },
-              {
-                score: 0.009407,
-                tone_id: 'disgust',
-                tone_name: 'Disgust'
-              },
-              {
-                score: 0.021511,
-                tone_id: 'fear',
-                tone_name: 'Fear'
-              },
-              {
-                score: 0.519797,
-                tone_id: 'joy',
-                tone_name: 'Joy'
-              },
-              {
-                score: 0.236706,
-                tone_id: 'sadness',
-                tone_name: 'Sadness'
-              }
-            ],
-            category_id: 'emotion_tone',
-            category_name: 'Emotion Tone'
+            score: 0.036792,
+            tone_id: 'anger',
+            tone_name: 'Anger'
           },
           {
-            tones: [
-              {
-                score: 0.883842,
-                tone_id: 'analytical',
-                tone_name: 'Analytical'
-              },
-              {
-                score: 0,
-                tone_id: 'confident',
-                tone_name: 'Confident'
-              },
-              {
-                score: 0.60858,
-                tone_id: 'tentative',
-                tone_name: 'Tentative'
-              }
-            ],
-            category_id: 'language_tone',
-            category_name: 'Language Tone'
+            score: 0.009407,
+            tone_id: 'disgust',
+            tone_name: 'Disgust'
           },
           {
-            tones: [
-              {
-                score: 0.538638,
-                tone_id: 'openness_big5',
-                tone_name: 'Openness'
-              },
-              {
-                score: 0.607415,
-                tone_id: 'conscientiousness_big5',
-                tone_name: 'Conscientiousness'
-              },
-              {
-                score: 0.742128,
-                tone_id: 'extraversion_big5',
-                tone_name: 'Extraversion'
-              },
-              {
-                score: 0.408756,
-                tone_id: 'agreeableness_big5',
-                tone_name: 'Agreeableness'
-              },
-              {
-                score: 0.669397,
-                tone_id: 'emotional_range_big5',
-                tone_name: 'Emotional Range'
-              }
-            ],
-            category_id: 'social_tone',
-            category_name: 'Social Tone'
+            score: 0.021511,
+            tone_id: 'fear',
+            tone_name: 'Fear'
+          },
+          {
+            score: 0.519797,
+            tone_id: 'joy',
+            tone_name: 'Joy'
+          },
+          {
+            score: 0.236706,
+            tone_id: 'sadness',
+            tone_name: 'Sadness'
           }
-        ]
+        ],
+        category_id: 'emotion_tone',
+        category_name: 'Emotion Tone'
       },
-      sentences_tone: [
+      {
+        tones: [
+          {
+            score: 0.883842,
+            tone_id: 'analytical',
+            tone_name: 'Analytical'
+          },
+          {
+            score: 0,
+            tone_id: 'confident',
+            tone_name: 'Confident'
+          },
+          {
+            score: 0.60858,
+            tone_id: 'tentative',
+            tone_name: 'Tentative'
+          }
+        ],
+        category_id: 'language_tone',
+        category_name: 'Language Tone'
+      },
+      {
+        tones: [
+          {
+            score: 0.538638,
+            tone_id: 'openness_big5',
+            tone_name: 'Openness'
+          },
+          {
+            score: 0.607415,
+            tone_id: 'conscientiousness_big5',
+            tone_name: 'Conscientiousness'
+          },
+          {
+            score: 0.742128,
+            tone_id: 'extraversion_big5',
+            tone_name: 'Extraversion'
+          },
+          {
+            score: 0.408756,
+            tone_id: 'agreeableness_big5',
+            tone_name: 'Agreeableness'
+          },
+          {
+            score: 0.669397,
+            tone_id: 'emotional_range_big5',
+            tone_name: 'Emotional Range'
+          }
+        ],
+        category_id: 'social_tone',
+        category_name: 'Social Tone'
+      }
+    ]
+  },
+  sentences_tone: [
+    {
+      sentence_id: 0,
+      text: 'I hope that we can reach an amicable understanding and a solution to this problem.',
+      input_from: 0,
+      input_to: 82,
+      tone_categories: [
         {
-          sentence_id: 0,
-          text: 'I hope that we can reach an amicable understanding and a solution to this problem.',
-          input_from: 0,
-          input_to: 82,
-          tone_categories: [
+          tones: [
             {
-              tones: [
-                {
-                  score: 0.0308,
-                  tone_id: 'anger',
-                  tone_name: 'Anger'
-                },
-                {
-                  score: 0.016122,
-                  tone_id: 'disgust',
-                  tone_name: 'Disgust'
-                },
-                {
-                  score: 0.107343,
-                  tone_id: 'fear',
-                  tone_name: 'Fear'
-                },
-                {
-                  score: 0.099391,
-                  tone_id: 'joy',
-                  tone_name: 'Joy'
-                },
-                {
-                  score: 0.44646,
-                  tone_id: 'sadness',
-                  tone_name: 'Sadness'
-                }
-              ],
-              category_id: 'emotion_tone',
-              category_name: 'Emotion Tone'
+              score: 0.0308,
+              tone_id: 'anger',
+              tone_name: 'Anger'
             },
             {
-              tones: [
-                {
-                  score: 0.897416,
-                  tone_id: 'analytical',
-                  tone_name: 'Analytical'
-                },
-                {
-                  score: 0,
-                  tone_id: 'confident',
-                  tone_name: 'Confident'
-                },
-                {
-                  score: 0.647986,
-                  tone_id: 'tentative',
-                  tone_name: 'Tentative'
-                }
-              ],
-              category_id: 'language_tone',
-              category_name: 'Language Tone'
+              score: 0.016122,
+              tone_id: 'disgust',
+              tone_name: 'Disgust'
             },
             {
-              tones: [
-                {
-                  score: 0.523542,
-                  tone_id: 'openness_big5',
-                  tone_name: 'Openness'
-                },
-                {
-                  score: 0.725246,
-                  tone_id: 'conscientiousness_big5',
-                  tone_name: 'Conscientiousness'
-                },
-                {
-                  score: 0.15085,
-                  tone_id: 'extraversion_big5',
-                  tone_name: 'Extraversion'
-                },
-                {
-                  score: 0.824779,
-                  tone_id: 'agreeableness_big5',
-                  tone_name: 'Agreeableness'
-                },
-                {
-                  score: 0.442224,
-                  tone_id: 'emotional_range_big5',
-                  tone_name: 'Emotional Range'
-                }
-              ],
-              category_id: 'social_tone',
-              category_name: 'Social Tone'
+              score: 0.107343,
+              tone_id: 'fear',
+              tone_name: 'Fear'
+            },
+            {
+              score: 0.099391,
+              tone_id: 'joy',
+              tone_name: 'Joy'
+            },
+            {
+              score: 0.44646,
+              tone_id: 'sadness',
+              tone_name: 'Sadness'
             }
-          ]
+          ],
+          category_id: 'emotion_tone',
+          category_name: 'Emotion Tone'
         },
         {
-          sentence_id: 1,
-          text: 'Our clients have done business together previously, and my client is interested in maintaining a profitable business- partner relationship with Mr. Compresi, assuming that your client can fulfill Mr. Farley’s order at the price promised in his letter.',
-          input_from: 83,
-          input_to: 334,
-          tone_categories: [
+          tones: [
             {
-              tones: [
-                {
-                  score: 0.05153,
-                  tone_id: 'anger',
-                  tone_name: 'Anger'
-                },
-                {
-                  score: 0.012946,
-                  tone_id: 'disgust',
-                  tone_name: 'Disgust'
-                },
-                {
-                  score: 0.01819,
-                  tone_id: 'fear',
-                  tone_name: 'Fear'
-                },
-                {
-                  score: 0.580387,
-                  tone_id: 'joy',
-                  tone_name: 'Joy'
-                },
-                {
-                  score: 0.155833,
-                  tone_id: 'sadness',
-                  tone_name: 'Sadness'
-                }
-              ],
-              category_id: 'emotion_tone',
-              category_name: 'Emotion Tone'
+              score: 0.897416,
+              tone_id: 'analytical',
+              tone_name: 'Analytical'
             },
             {
-              tones: [
-                {
-                  score: 0.550576,
-                  tone_id: 'analytical',
-                  tone_name: 'Analytical'
-                },
-                {
-                  score: 0,
-                  tone_id: 'confident',
-                  tone_name: 'Confident'
-                },
-                {
-                  score: 0,
-                  tone_id: 'tentative',
-                  tone_name: 'Tentative'
-                }
-              ],
-              category_id: 'language_tone',
-              category_name: 'Language Tone'
+              score: 0,
+              tone_id: 'confident',
+              tone_name: 'Confident'
             },
             {
-              tones: [
-                {
-                  score: 0.570278,
-                  tone_id: 'openness_big5',
-                  tone_name: 'Openness'
-                },
-                {
-                  score: 0.747083,
-                  tone_id: 'conscientiousness_big5',
-                  tone_name: 'Conscientiousness'
-                },
-                {
-                  score: 0.88372,
-                  tone_id: 'extraversion_big5',
-                  tone_name: 'Extraversion'
-                },
-                {
-                  score: 0.252507,
-                  tone_id: 'agreeableness_big5',
-                  tone_name: 'Agreeableness'
-                },
-                {
-                  score: 0.770672,
-                  tone_id: 'emotional_range_big5',
-                  tone_name: 'Emotional Range'
-                }
-              ],
-              category_id: 'social_tone',
-              category_name: 'Social Tone'
+              score: 0.647986,
+              tone_id: 'tentative',
+              tone_name: 'Tentative'
             }
-          ]
+          ],
+          category_id: 'language_tone',
+          category_name: 'Language Tone'
+        },
+        {
+          tones: [
+            {
+              score: 0.523542,
+              tone_id: 'openness_big5',
+              tone_name: 'Openness'
+            },
+            {
+              score: 0.725246,
+              tone_id: 'conscientiousness_big5',
+              tone_name: 'Conscientiousness'
+            },
+            {
+              score: 0.15085,
+              tone_id: 'extraversion_big5',
+              tone_name: 'Extraversion'
+            },
+            {
+              score: 0.824779,
+              tone_id: 'agreeableness_big5',
+              tone_name: 'Agreeableness'
+            },
+            {
+              score: 0.442224,
+              tone_id: 'emotional_range_big5',
+              tone_name: 'Emotional Range'
+            }
+          ],
+          category_id: 'social_tone',
+          category_name: 'Social Tone'
+        }
+      ]
+    },
+    {
+      sentence_id: 1,
+      text: 'Our clients have done business together previously, and my client is interested in maintaining a profitable business- partner relationship with Mr. Compresi, assuming that your client can fulfill Mr. Farley’s order at the price promised in his letter.',
+      input_from: 83,
+      input_to: 334,
+      tone_categories: [
+        {
+          tones: [
+            {
+              score: 0.05153,
+              tone_id: 'anger',
+              tone_name: 'Anger'
+            },
+            {
+              score: 0.012946,
+              tone_id: 'disgust',
+              tone_name: 'Disgust'
+            },
+            {
+              score: 0.01819,
+              tone_id: 'fear',
+              tone_name: 'Fear'
+            },
+            {
+              score: 0.580387,
+              tone_id: 'joy',
+              tone_name: 'Joy'
+            },
+            {
+              score: 0.155833,
+              tone_id: 'sadness',
+              tone_name: 'Sadness'
+            }
+          ],
+          category_id: 'emotion_tone',
+          category_name: 'Emotion Tone'
+        },
+        {
+          tones: [
+            {
+              score: 0.550576,
+              tone_id: 'analytical',
+              tone_name: 'Analytical'
+            },
+            {
+              score: 0,
+              tone_id: 'confident',
+              tone_name: 'Confident'
+            },
+            {
+              score: 0,
+              tone_id: 'tentative',
+              tone_name: 'Tentative'
+            }
+          ],
+          category_id: 'language_tone',
+          category_name: 'Language Tone'
+        },
+        {
+          tones: [
+            {
+              score: 0.570278,
+              tone_id: 'openness_big5',
+              tone_name: 'Openness'
+            },
+            {
+              score: 0.747083,
+              tone_id: 'conscientiousness_big5',
+              tone_name: 'Conscientiousness'
+            },
+            {
+              score: 0.88372,
+              tone_id: 'extraversion_big5',
+              tone_name: 'Extraversion'
+            },
+            {
+              score: 0.252507,
+              tone_id: 'agreeableness_big5',
+              tone_name: 'Agreeableness'
+            },
+            {
+              score: 0.770672,
+              tone_id: 'emotional_range_big5',
+              tone_name: 'Emotional Range'
+            }
+          ],
+          category_id: 'social_tone',
+          category_name: 'Social Tone'
         }
       ]
     }
   ]
 }
+
+export const mockMergedTonesArray = [
+  {"score": 0.0308, "tone_id": "anger", "tone_name": "Anger"}, 
+  {"score": 0.016122, "tone_id": "disgust", "tone_name": "Disgust"}, 
+  {"score": 0.107343, "tone_id": "fear", "tone_name": "Fear"}, 
+  {"score": 0.099391, "tone_id": "joy", "tone_name": "Joy"}, 
+  {"score": 0.44646, "tone_id": "sadness", "tone_name": "Sadness"}, 
+  {"score": 0.897416, "tone_id": "analytical", "tone_name": "Analytical"}, 
+  {"score": 0, "tone_id": "confident", "tone_name": "Confident"}, 
+  {"score": 0.647986, "tone_id": "tentative", "tone_name": "Tentative"}
+]
+
--- src/dataHelper/dataHelper.test.js
+++ src/dataHelper/dataHelper.test.js
@@ -1,5 +1,5 @@
-import { toneFetch, mockFetch } from './dataHelper'
-import { mockText, mockExpected, mockResponse } from '../__mocks__/mockData'
+import { toneFetch, mockFetch, cleanTones, cleanDocumentTone, cleanSentences, cleanTone, cleanToneCategories, filterAndSort } from './dataHelper'
+import { mockText, mockExpected, mockResponse, mockCleaned, mockUncleaned, mockMergedTonesArray } from '../__mocks__/mockData'
 
 describe('dataHelper', () => {
 
@@ -72,6 +72,63 @@ describe('dataHelper', () => {
       const expected = Error("Unable to fetch mock data")
 
       expect(response).rejects.toEqual(expected)
+    })
+  })
+
+  describe('cleanTones', () => {
+    it('should return an object with the expected cleaned data', () => {
+      const result = cleanTones(mockUncleaned)
+
+      expect(result).toEqual(mockCleaned)
+    })
+  })
+
+  describe('cleanDocumentTone', () => {
+    it('should return an array of expected cleaned document tone objects', () => {
+      const result = cleanDocumentTone(mockUncleaned)
+      const expected = mockCleaned.documentTone
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('cleanSentences', () => {
+    it('should return the expected array of cleaned sentence objects', () => {
+      const result = cleanSentences(mockUncleaned)
+      const expected = mockCleaned.sentences
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('cleanTone', () => {
+    it('should return the expected array of tone objects', () => {
+      const result = cleanTone(mockUncleaned.sentences_tone[0], 0.2)
+      const expected = mockCleaned.sentences[0].tones
+
+      expect(result).toEqual(expected)
+
+    })
+  })
+
+  describe('cleanToneCategories', () => {
+    it('should return the expected merged array of tone objects', () => {
+      const result = cleanToneCategories(mockUncleaned.sentences_tone[0].tone_categories)
+
+      expect(result).toEqual(mockMergedTonesArray)
+    })
+  })
+
+  describe('filterAndSort', () => {
+    it('should return the filtered and sorted array of tone objects', () => {
+      const result = filterAndSort(mockMergedTonesArray, .3)
+      const expected = [
+        {"score": 0.897416, "tone_id": "analytical", "tone_name": "Analytical"}, 
+        {"score": 0.647986, "tone_id": "tentative", "tone_name": "Tentative"}, 
+        {"score": 0.44646, "tone_id": "sadness", "tone_name": "Sadness"}
+      ]
+
+      expect(result).toEqual(expected)
     })
   })
 })
