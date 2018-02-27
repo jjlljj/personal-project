9dad5ad78263e4747fd62d35ca6dca275eb56913
James Logue <logueja@gmail.com>
2018-02-27 07:49:20 -0700

    Style toneCard headers

--- src/components/ToneCard/ToneCard.js
+++ src/components/ToneCard/ToneCard.js
@@ -3,11 +3,19 @@ import './ToneCard.css'
 
 export const ToneCard = ({ tone_name, description, score = "" }) => {
 
+  const renderedScore = score && (
+      <h4>{score}</h4>
+    ) || ""
+
   return (
-    <div>
-      <h3>{tone_name}</h3>
+    <div className="tone-card">
+      <div className={ "tone-title-wrap background-" + tone_name }>
+
+        <h3>{tone_name}</h3>
+        <div className="overlay"></div> 
+      </div>
+      { renderedScore }
       <p>{description}</p>
-      I am tonecard
     </div>
   )
 }
